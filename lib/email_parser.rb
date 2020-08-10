714df618f497e4c95edb36bfbca4c8633da2d064
Percy Major <chipercy@hotmail.com>
2020-08-10 04:10:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,9 +14,9 @@ class EmailAddressParser
 end
  
  
- "john@doe.com, person@somewhere.org"
-parser = EmailAddressParser.new(email_addresses)
+# "john@doe.com, person@somewhere.org"
+# parser = EmailAddressParser.new(email_addresses)
  
-parser.parse
-# => ["john@doe.com", "person@somewhere.org"]
+# parser.parse
+# # => ["john@doe.com", "person@somewhere.org"]
  
